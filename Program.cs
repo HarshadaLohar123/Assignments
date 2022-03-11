a01688ba832f3e51ced78ad2106ec4e8893d0f51
harshadalohar123 <harshalohar123@gmail.com>
2022-03-11 12:37:38 +0530

    ADDED:FlipCoin And LeapYear

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            obj.LeapYear();
+            obj.HeadTail();
+        }
+    }
+}
